c0b9ca3f655253fd93e99a9b05a9553ef4777a33
atomi19 <175505103+atomi19@users.noreply.github.com>
2026-01-29 21:44:16 +0200

    Change appearance for pickDate (showDatePicker) and pickTime (showTimePicker)

--- lib/widgets/date_time_picker.dart
+++ lib/widgets/date_time_picker.dart
@@ -6,9 +6,33 @@ Future<DateTime?> pickDate({
   }) {
   return showDatePicker(
     context: context, 
+    switchToCalendarEntryModeIcon: const Icon(Icons.calendar_month),
+    switchToInputEntryModeIcon: const Icon(Icons.edit_outlined),
+    initialEntryMode: DatePickerEntryMode.calendarOnly,
     firstDate: DateTime(2000), 
     currentDate: currentDate,
-    lastDate: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day)
+    lastDate: DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day),
+    builder: (context, child) {
+      return Theme(
+        data: Theme.of(context).copyWith(
+          datePickerTheme: DatePickerThemeData(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          ),
+          colorScheme: const ColorScheme.light(
+            primary: Colors.blue,
+            onPrimary: Colors.white,
+            onSurface: Colors.black,
+          ),
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.grey.shade200,
+            ),
+          ),
+        ), 
+        child: child!
+      );
+    }
   );
 }
 
@@ -20,5 +44,29 @@ Future<TimeOfDay?> pickTime({
     context: context, 
     initialEntryMode: TimePickerEntryMode.inputOnly,
     initialTime: initialTime ?? TimeOfDay.now(),
+    builder: (context, child) {
+      return Theme(
+        data: Theme.of(context).copyWith(
+          timePickerTheme: TimePickerThemeData(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          ),
+          colorScheme: ColorScheme.light(
+            primary: Colors.grey.shade50, // selected hour/minute bg color
+            onPrimary: Colors.black, // selected hour/minute text color
+            onSurface: Colors.black, // not selected hour/minute text color
+          ),
+          dialogTheme: const DialogThemeData(
+            backgroundColor: Colors.white,
+          ),
+          textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.grey.shade200,
+            )
+          )
+        ), 
+        child: child!
+      );
+    }
   );
 }

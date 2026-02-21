994a58a12a3f01a46438fc42af8d8fd704182022
atomi19 <175505103+atomi19@users.noreply.github.com>
2026-02-21 22:38:24 +0200

    Fix: enable modalBottomSheet useSafeArea to prevent content from being hidden under system ui

--- lib/widgets/custom_modal_bottom_sheet.dart
+++ lib/widgets/custom_modal_bottom_sheet.dart
@@ -12,22 +12,21 @@ void showCustomModalBottomSheet({
   }) {
   showModalBottomSheet(
     context: context, 
+    useSafeArea: true,
     isScrollControlled: isScrollControlled ?? false,
     backgroundColor: backgroundColor ?? Colors.grey.shade200,
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadiusGeometry.vertical(top: Radius.circular(borderRadius ?? 15))
     ),
     builder: (context) {
-      return SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(
-            left: padding ?? 10,
-            top: padding ?? 10,
-            right: padding ?? 10,
-            bottom: MediaQuery.of(context).viewInsets.bottom +10,
-          ),
-          child: child,
-        )
+      return Padding(
+        padding: EdgeInsets.only(
+          left: padding ?? 10,
+          top: padding ?? 10,
+          right: padding ?? 10,
+          bottom: MediaQuery.of(context).viewInsets.bottom +10,
+        ),
+        child: child,
       );
     }
   );
